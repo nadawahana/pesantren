c0f045b219ebfe63d36a6e9c4624f263a53e3e72
Andra Hotmartua Al Hafiz HSB <52515217+andrahafiz@users.noreply.github.com>
2023-07-24 21:57:23 +0700

    remove informasi pendaftaran

--- resources/views/admindaftar.blade.php
+++ resources/views/admindaftar.blade.php
@@ -15,7 +15,7 @@
             <div class="col-md-12 mb-3">
                 <div class="row">
                     <div class="col-12">
-                        <h3 class="font-weight-bold">Informasi Pendaftaran</h3>
+                        {{-- <h3 class="font-weight-bold">Informasi Pendaftaran</h3> --}}
                     </div>
                 </div>
             </div>
--- resources/views/gelombang1.blade.php
+++ resources/views/gelombang1.blade.php
@@ -10,7 +10,7 @@
             <div class="col-md-12 grid-margin">
                 <div class="row">
                     <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                        <h3 class="font-weight-bold">Informasi Pendaftaran</h3>
+                        {{-- <h3 class="font-weight-bold">Informasi Pendaftaran</h3> --}}
                         <h6 class="font-weight-normal mb-0">Silahkan <a href="/inputdaftar"><span class="text-primary"
                                     type="submit">Klik disini!</span></a> untuk pembayaran pendaftaran</h6>
                     </div>
